95cf51c5cbba88698d74d98540d4cd463a1f64e7
Mohammedasif Pinitod <87132170+MohammedasifP@users.noreply.github.com>
2022-04-07 19:56:50 +0530

    payment filtering completed

--- src/Components/RestaurantDetails/RestaurantDetails.jsx
+++ src/Components/RestaurantDetails/RestaurantDetails.jsx
@@ -5,7 +5,8 @@ import './RestaurantDetails.css'
 export const RestaurantDetails=()=>{
 const [resdata,setResdata]=useState([]);
 const [page,setpage]=useState(1); 
-const [star,setStar]=useState(null)   
+const [star,setStar]=useState(null); 
+const [payment,setPayment]=useState("");
 
  useEffect(()=>{
    getdata()
@@ -23,8 +24,17 @@ const Starfun=(value)=>{
 const getstar=()=>{
     fetch(`http://localhost:3000/Restaurants?rating=${star}`).then(Response=>Response.json()).then(data=>setResdata(data))
 } 
+ 
+const paymentfun=(method)=>{
+   setPayment(method)
+   getpayment()
+}
+const getpayment=()=>{
+    fetch(`http://localhost:3000/Restaurants?Payment_methods=${payment}`).then(Response=>Response.json()).then(data=>setResdata(data))
+
+}
 // http://localhost:3000/Restaurants?_sort=name&_order=asc
-// http://localhost:3000/Restaurants?rating=${1}
+
 return(
     <div>
       <div className="pagination">
@@ -40,6 +50,12 @@ return(
       <button onClick={()=>{Starfun(4)}}>4 star</button>
       <button onClick={()=>{Starfun(5)}}>5 star</button>
       </div>
+      <div className="method">
+          <button onClick={()=>{paymentfun("cash")}}>cash</button>
+          <button onClick={()=>{paymentfun("card")}}>card</button>
+          <button onClick={()=>{getdata()}}>all</button>
+      </div>
+      
         {resdata.map((elem)=>{
             return (
               <div>
